c826d487fefe7edb495d166578fd1fe972c17512
tmahmood-42 <59780873+tmahmood-42@users.noreply.github.com>
2023-03-31 15:02:16 -0700

    Update petshelter1_lab1.cpp

--- petshelter1_lab1.cpp
+++ petshelter1_lab1.cpp
@@ -1,6 +1,6 @@
 
 
-//Program Written by Tahir Mahmood
+//Program Written by and Tahir
 
 // Practice program to review
 //loops, functions, dynamic allocation,
@@ -158,101 +158,104 @@ int main()
     {
         std::string filename;
         std::ifstream input_file;
-    do
-    {
-        std::cout << "Please enter the name of the file to read in: ";
-        std::cin >> filename;
-        input_file.open(filename);
-        if (!input_file)
+        do
         {
-            std::cerr << "Error opening file, Please try again." << std::endl;
-        }
-    } 
-    while (!input_file);
+            std::cout << "Please enter the name of the file to read in: ";
+            std::cin >> filename;
+            input_file.open(filename);
+            if (!input_file)
+            {
+                std::cerr << "Error opening file, Please try again." << std::endl;
+            }
+        } 
+        while (!input_file);
 
 
-   // Initialize variables
-int last_num = 0; // This variable will store the last number in a cat ID
-std::string last_line; // This variable will store each line read from the input file
-
-// Read the input file line by line
-while (std::getline(input_file, last_line))
-{
-// Parse the current line into individual fields
-std::stringstream ss(last_line);
-std::string new_id, new_name, new_hair, new_color;
-int days_in_shelter;
-ss >> new_id >> new_name >> new_hair >> new_color >> days_in_shelter;
-
-
-// Check if the ID of the current cat is greater than the last number
-if (new_id.length() > 3 && isdigit(new_id[new_id.length() - 1]))
-{
-    last_num = std::max(last_num, stoi(new_id.substr(3)));
-}
-
-// If there is still space in the array of cats, create a new cat object and add it to the array
-if (count < NUM_CATS)
-{
-    shelter[count] = Cat(new_name, new_hair, new_color, new_id);
-    count++;
-}
-// Otherwise, break out of the loop and output an error message
-else
-{
-    std::cout << "The file contains the maximum number of cats. You cannot add more cats." << std::endl;
-    break;
-}
-}
-    
-    input_file.close();
-    if (count < NUM_CATS)
-    {
-        std::cout << "There are " << NUM_CATS - count << " spots left for input." << std::endl;
-    }
-    else
-    {
-        std::cout << "The shelter is full. You cannot add more cats." << std::endl;
-    }
-
-    while (count < NUM_CATS)
-    {
-        std::string new_name, new_hair, new_color;
-        std::cout << "Please enter the pet's name, hair type, and color or -1 to exit: " << std::endl;
-        std::cout << "Name: ";
-        std::cin >> new_name;
-        if (new_name == "-1")
-        {
-            break;
-        }
-        std::cout << "Hair type: ";
-        std::cin >> new_hair;
-        std::cout << "Color: ";
-        std::cin >> new_color;
+        // Initialize variables
+        int last_num = 0; // This variable will store the last number in a cat ID
+        std::string last_line; // This variable will store each line read from the input file
         
-        std::string new_id = "CAT" + std::to_string(last_num + 1);
-        shelter[count] = Cat(new_name, new_hair, new_color, new_id);
-        count++;
-        last_num++;
-    }
-
-    std::cout << "Please enter the name of the file to write out: ";
-    std::cin >> filename;
-
-    std::ofstream output_file(filename);
-
-    for (int i = 0; i < count; i++)
-    {
-        shelter[i].increment_days_in_shelter();
-     output_file << shelter[i].get_id() << " " << shelter[i].get_name() << " " << shelter[i].get_hair() << " " << shelter[i].get_color()
-     << " " << shelter[i].get_days_in_shelter() << std::endl;
-    }
-
-    output_file.close();
-    std::cout << "Saved " << count << " cats to " << filename << std::endl;
-    return 0;
+        // Read the input file line by line
+        while (std::getline(input_file, last_line))
+        {
+            // Parse the current line into individual fields
+            std::stringstream ss(last_line);
+            std::string new_id, new_name, new_hair, new_color;
+            int days_in_shelter;
+            ss >> new_id >> new_name >> new_hair >> new_color >> days_in_shelter;
+            
+            
+            // Check if the ID of the current cat is greater than the last number
+            if (new_id.length() > 3 && isdigit(new_id[new_id.length() - 1]))
+            {
+                last_num = std::max(last_num, stoi(new_id.substr(3)));
+            }
+            
+            // If there is still space in the array of cats, create a new cat object and add it to the array
+            if (count < NUM_CATS)
+            {
+                shelter[count] = Cat(new_name, new_hair, new_color, new_id);
+                count++;
+            }
+            // Otherwise, break out of the loop and output an error message
+            else
+            {
+                std::cout << "The file contains the maximum number of cats. You cannot add more cats." << std::endl;
+                break;
+            }
+        
+        }
+        
+        input_file.close();
+        
+        if (count < NUM_CATS)
+        {
+            std::cout << "There are " << NUM_CATS - count << " spots left for input." << std::endl;
+        }
+        else
+        {
+            std::cout << "The shelter is full. You cannot add more cats." << std::endl;
+        }
+    
+        while (count < NUM_CATS)
+        {
+            std::string new_name, new_hair, new_color;
+            std::cout << "Please enter the pet's name, hair type, and color or -1 to exit: " << std::endl;
+            std::cout << "Name: ";
+            std::cin >> new_name;
+            if (new_name == "-1")
+            {
+                break;
+            }
+            std::cout << "Hair type: ";
+            std::cin >> new_hair;
+            std::cout << "Color: ";
+            std::cin >> new_color;
+            
+            std::string new_id = "CAT" + std::to_string(last_num + 1);
+            shelter[count] = Cat(new_name, new_hair, new_color, new_id);
+            count++;
+            last_num++;
+        }
+    
+        std::cout << "Please enter the name of the file to write out: ";
+        std::cin >> filename;
+    
+        std::ofstream output_file(filename);
+    
+        for (int i = 0; i < count; i++)
+        {
+            shelter[i].increment_days_in_shelter();
+            output_file << shelter[i].get_id() << " " << shelter[i].get_name() << " " << shelter[i].get_hair() << " " << shelter[i].get_color()
+            << " " << shelter[i].get_days_in_shelter() << std::endl;
+        }
+        
+        output_file.close();
+        std::cout << "Saved " << count << " cats to " << filename << std::endl;
+        
+        return 0;
+        
     }//end bracket fo first if statement
-
    
     /****************************
     End of Path A (File Input)
@@ -313,4 +316,3 @@ else
 }
     
     
-   
